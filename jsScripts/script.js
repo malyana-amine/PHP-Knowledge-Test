b98f0567bc23614cd7ba8c0eb1ff0093048d2943
amine-malyana <aminemalyana12@gmail.com>
2022-12-25 18:04:01 +0100

    finich partie de style (sass)

--- jsScripts/script.js
+++ jsScripts/script.js
@@ -1,11 +1,16 @@
 let btnstar = document.querySelector('.startBtn');
 let pageInfo = document.querySelector('.pageInfo');
 let pageQue = document.querySelector('.pageQue');
+let pageQue1 = document.querySelector('.pageQue1');
 let pageRes = document.querySelector('.pageRes');
 let exitBtn = document.querySelector('.exitBtn');
 let nxtBtn = document.querySelector('.nxtBtn');
+
+let resBtn = document.querySelector('.resBtn');
+
 let quescoun = document.querySelector('.quescoun');
 let answercount = document.querySelector('.answercount');
+let options = document.querySelectorAll(".option");
 
 // let choise_btn1 = document.querySelector('.choise_btn1');
 // let choise_btn2= document.querySelector('.choise_btn2');
@@ -17,49 +22,24 @@ let choise_btn21= document.querySelector('.choise_btn21');
 let choise_btn31 = document.querySelector('.choise_btn31');
 let choise_btn41 = document.querySelector('.choise_btn41');
 
-// choise_btn1.onclick= ()=>{
-//     choise_btn1.disabled = true;
-//     choise_btn2.disabled = true;
-//     choise_btn3.disabled = true;
-//     choise_btn4.disabled = true;
-// }
-// choise_btn2.onclick= ()=>{
-//     choise_btn1.disabled = true;
-//     choise_btn2.disabled = true;
-//     choise_btn3.disabled = true;
-//     choise_btn4.disabled = true;
-// }
-// choise_btn3.onclick= ()=>{
-//     choise_btn1.disabled = true;
-//     choise_btn2.disabled = true;
-//     choise_btn3.disabled = true;
-//     choise_btn4.disabled = true;
-// }
-// choise_btn4.onclick= ()=>{
-//     choise_btn1.disabled = true;
-//     choise_btn2.disabled = true;
-//     choise_btn3.disabled = true;
-//     choise_btn4.disabled = true;
-// }
-
-
-
 
 
 
 btnstar.onclick = ()=>{
     pageInfo.classList.add("hidden");
     pageInfo.classList.remove("pageInfo");
-    pageQue.classList.remove("hidden");
+    pageQue1.classList.remove("hidden");
+    pageQue1.classList.add("pageQue");
 
     showQue(generateRandomNumber());
     quesNum(1);
 }
 exitBtn.onclick = ()=>{
+    pageQue1.classList.remove("pageQue");
     pageInfo.classList.add("pageInfo");
     nxtBtn.classList.add("hidden");
     pageInfo.classList.remove("hidden");
-    pageQue.classList.add("hidden");
+    pageQue1.classList.add("hidden");
     // generateRandomNumber();
     usedNumbers=[];
 
@@ -69,44 +49,28 @@ exitBtn.onclick = ()=>{
     choise_btn41.disabled = false;
     que_count=0;
     que_num = 1;
+
+    options.forEach(bonneAns=>{
+        bonneAns.classList.remove("bonneAns");
+    })
+
+    options.forEach(mauvAns=>{
+        mauvAns.classList.remove("mauvAns");
+    })
+
+
+    options.forEach(hover=>{
+        hover.classList.add("hover");
+    })
+    options.forEach(hover=>{
+        hover.classList.remove("border");
+    })
 }
 
 let choice1 = document.querySelector('.choice1');
 let choice2 = document.querySelector('.choice2');
 let choice3 = document.querySelector('.choice3');
 let choice4 = document.querySelector('.choice4');
-
-
-
-// choice1.onclick = ()=>{
-//    nxtBtn.classList.remove("hidden");
-//    choise_btn11.disabled = true;
-//    choise_btn21.disabled = true;
-//    choise_btn31.disabled = true;
-//    choise_btn41.disabled = true;
-// }
-// choice2.onclick = ()=>{
-//     nxtBtn.classList.remove("hidden");
-//     choise_btn11.disabled = true;
-//     choise_btn21.disabled = true;
-//     choise_btn31.disabled = true;
-//     choise_btn41.disabled = true;
-//  }
-//  choice3.onclick = ()=>{
-//     nxtBtn.classList.remove("hidden");
-//     choise_btn11.disabled = true;
-//     choise_btn21.disabled = true;
-//     choise_btn31.disabled = true;
-//     choise_btn41.disabled = true;
-//  }
-//  choice4.onclick = ()=>{
-//     nxtBtn.classList.remove("hidden");
-//     choise_btn11.disabled = true;
-//     choise_btn21.disabled = true;
-//     choise_btn31.disabled = true;
-//     choise_btn41.disabled = true;
-//  }
-
 
 
 let usedNumbers = []
@@ -118,9 +82,9 @@ function generateRandomNumber() {
   } else {
     usedNumbers.push(randomNumber);    
   }
-//   console.log(randomNumber);
   return randomNumber;
 }
+
 
 let que_count = 0 ;
 let que_num = 1;
@@ -131,25 +95,45 @@ nxtBtn.onclick = ()=>{
         que_num++;
         quesNum(que_num);
 
-
-        // choise_btn1.disabled = false;
-        // choise_btn2.disabled = false;
-        // choise_btn3.disabled = false;
-        // choise_btn4.disabled = false;
-
         choise_btn11.disabled = false;
+        // choise_btn11.classList.remove("bonneAns");
+        // choise_btn11.classList.remove("mauvAns");
         choise_btn21.disabled = false;
+        // choise_btn21.classList.remove("bonneAns");
+        // choise_btn21.classList.remove("mauvAns");
         choise_btn31.disabled = false;
+        // choise_btn31.classList.remove("bonneAns");
+        // choise_btn31.classList.remove("mauvAns");
         choise_btn41.disabled = false;
+        // choise_btn41.classList.remove("bonneAns");
+        // choise_btn41.classList.remove("mauvAns");
 
+        options.forEach(option=>{
+            option.classList.remove("bonneAns");
+            option.classList.remove("mauvAns");
+        })
+
+
+        options.forEach(hover=>{
+            hover.classList.add("hover");
+        })
+        options.forEach(hover=>{
+            hover.classList.remove("border");
+        })
         
         showQue(generateRandomNumber());
     }else{
         pageRes.classList.remove("hidden");
-        pageQue.classList.remove("pageQue");
-        pageQue.classList.add("hidden");
+        pageQue1.classList.remove("pageQue");
+        pageQue1.classList.add("hidden");
     }
 }
+resBtn.addEventListener("click",()=>{
+    pageRes.classList.remove("hidden");
+    pageQue1.classList.remove("pageQue");
+    pageQue1.classList.add("hidden");
+}
+)
 
 
 let showQue = (index)=>{
@@ -179,18 +163,38 @@ let showQue = (index)=>{
 let answerCount = 0;
 let optionselected = (answer1)=>{
     let userUns = answer1.value
+    if(que_num<10){
     nxtBtn.classList.remove("hidden");
+}else{
+    resBtn.classList.remove("hidden");
+}
     choise_btn11.disabled = true;
     choise_btn21.disabled = true;
     choise_btn31.disabled = true;
     choise_btn41.disabled = true;
+    options.forEach(hover=>{
+        hover.classList.remove("hover");
+    }
+
+    )
+    answer1.classList.add("border")
     console.log(userUns);
     if (userUns == questions[randomNumber].answer){
             answerCount++;
             unswerNum(answerCount);
+            // answer1.classList.add("bonneAns");
             console.log(answerCount)
+
             console.log(questions[randomNumber].answer)
     }
+
+
+    options.forEach(option=>{
+        if(option.value==questions[randomNumber].answer){
+            option.classList.add("bonneAns")
+        }
+        else option.classList.add("mauvAns")
+    })
     }
 
 
